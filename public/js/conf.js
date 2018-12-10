fea9ff3939bc75a78ada59608dfcb3104f39bb2f
Cristian Mircea Messel <mess110@gmail.com>
2018-12-10 20:24:41 +0200

    allow creation of custom rooms

--- public/js/conf.js
+++ public/js/conf.js
@@ -1,9 +1,44 @@
 var cm
 
+// https://stackoverflow.com/a/2117523
+function guid() {
+  if (crypto !== undefined && crypto !== null && typeof(crypto.getRandomValues) === 'function') {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+  } else {
+    logger.warn("Using Math.random which is not really random")
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
+  }
+}
+
+function newRoom() {
+  window.location.href = getRoomUrl()
+}
+
+function getRoomUrl(id) {
+  if (id === null || id === undefined || id === '') {
+    id = guid()
+  }
+  let base = window.location.href.split('#')[0]
+  base = base.split('?')[0]
+  return base + `?id=${id}`
+}
+
 function init() {
   var container = document.getElementById('container')
   var classes = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
   var clients = 1
+  var iceServers = [
+    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
+  ]
+  var roomId = discoveryClient.getParameterByName('id') || 'conf-default'
+
+  var roomIdLink = document.querySelector('#roomId')
+  roomIdLink.textContent = roomId
+  roomIdLink.setAttribute('href', getRoomUrl(roomId))
 
   navigator.mediaDevices.enumerateDevices().then((devices) => {
     let mediaOptions = {
@@ -30,13 +65,6 @@ function init() {
 
   function initRTC(stream) {
     play('own', stream)
-
-    let roomId = discoveryClient.getParameterByName('id') || 'conf-default'
-
-    let iceServers = [
-      { urls: 'stun:stun.l.google.com:19302' },
-      { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
-    ]
     cm = new discoveryClient.Mesh(handlePeer, iceServers)
     cm.connect('/', roomId, stream)
   }

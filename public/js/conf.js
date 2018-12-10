0b424130925509e9c733cb6e77ec32c353c76bfb
Cristian Mircea Messel <mess110@gmail.com>
2018-12-10 19:21:50 +0200

    cleanup isle 1

--- public/js/conf.js
+++ public/js/conf.js
@@ -1,83 +1,100 @@
-var cm;
-var classes = ["one","two","three","four","five","six","seven","eight"];
-var clients = 1;
-
-function getFullId(id) {
-  return `video-${id}`
-}
+var cm
 
 function init() {
-  let container = document.getElementById('container');
+  var container = document.getElementById('container')
+  var classes = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
+  var clients = 1
 
-  navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(stream) {
-    initRTC(stream);
-  }).catch(function(err) {
-    console.log(err)
-    initRTC(false);
-  });
-
-  function findOrCreateVideo(id) {
-    let container = document.getElementById('container');
-    container.className = classes[clients-1];
-
-    var v = document.getElementById(getFullId(id));
-    if (v === null) {
-      var item = document.createElement("div");
-      item.className = "item";
-      v = document.createElement('video');
-      v.setAttribute('id', getFullId(id));
-      v.setAttribute('class', 'video-item');
-      v.muted = id == 'own';
-      item.appendChild(v);
-      container.appendChild(item);
-      clients++;
+  navigator.mediaDevices.enumerateDevices().then((devices) => {
+    let mediaOptions = {
+      video: false,
+      audio: false
     }
-    return v;
-  }
+    devices.forEach((device) => {
+      if (device.kind === 'videoinput') { mediaOptions.video = true }
+      if (device.kind === 'audioinput') { mediaOptions.audio = true }
+    })
 
-  function handlePeer(peer) {
-    peer.on('connect', function () {
-      console.log('connect');
-      findOrCreateVideo(peer._id);
-    });
-    peer.on('data', function (data) {
-      console.log(data);
-    });
-    peer.on('stream', function (stream) {
-      console.log('stream');
-      var v = findOrCreateVideo(peer._id);
-      v.srcObject = stream;
-      v.play();
-    });
-    peer.on('error', function (error) {
-      console.error(error);
-    });
-    peer.on('close', function () {
-      console.log('close');
-      var v = document.getElementById(getFullId(peer._id));
-      if (v !== null) {
-        container.removeChild(v.parentElement);
-        clients--;
-      }
-    });
+    initHardware(mediaOptions)
+  })
+
+  function initHardware(options) {
+    console.log(options)
+    navigator.mediaDevices.getUserMedia(options).then((stream) => {
+      initRTC(stream)
+    }).catch((error) => {
+      console.error(error)
+      initRTC(false)
+    })
   }
 
   function initRTC(stream) {
-    var video = findOrCreateVideo('own');
-    if (stream) {
-      video.srcObject = stream;
-      video.play()
-    }
+    play('own', stream)
 
-    var roomId = discoveryClient.getParameterByName('id') || 'conf-default';
+    let roomId = discoveryClient.getParameterByName('id') || 'conf-default'
 
     let iceServers = [
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
     ]
-    cm = new discoveryClient.Mesh(handlePeer, iceServers);
-    cm.connect('/', roomId, stream);
+    cm = new discoveryClient.Mesh(handlePeer, iceServers)
+    cm.connect('/', roomId, stream)
+  }
+
+  function getFullId(id) {
+    return `video-${id}`
+  }
+
+  function findOrCreateVideo(id) {
+    container.className = classes[clients-1]
+
+    let v = document.getElementById(getFullId(id))
+    if (v === null) {
+      let item = document.createElement("div")
+      item.className = "item"
+      v = document.createElement('video')
+      v.setAttribute('id', getFullId(id))
+      v.setAttribute('class', 'video-item')
+      v.muted = id == 'own'
+      item.appendChild(v)
+      container.appendChild(item)
+      clients++
+    }
+    return v
+  }
+
+  function handlePeer(peer) {
+    peer.on('connect', () => {
+      console.log('connect')
+      findOrCreateVideo(peer._id)
+    })
+    peer.on('data', (data) => {
+      console.log(data)
+    })
+    peer.on('stream', (stream) => {
+      console.log('stream')
+      play(peer._id, stream)
+    })
+    peer.on('error', (error) => {
+      console.error(error)
+    })
+    peer.on('close', () => {
+      console.log('close')
+      let v = document.getElementById(getFullId(peer._id))
+      if (v !== null) {
+        container.removeChild(v.parentElement)
+        clients--
+      }
+    })
+  }
+
+  function play(id, stream) {
+    let video = findOrCreateVideo(id)
+    if (stream) {
+      video.srcObject = stream
+      video.play()
+    }
   }
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", init)
